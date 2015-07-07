29fa4e42d42bddcddfec0ce9542fc8eb78557c38
dalyd <david.daly@mongodb.com>
2015-07-07 16:03:32 -0400

    Updated process_results.py for new output file. Updated per_test_data and added function to load data

--- scripts/process_results.py
+++ scripts/process_results.py
@@ -3,38 +3,46 @@
 import pymongo
 import math
 import subprocess
+import numpy
+import json
 
 client = pymongo.MongoClient()
+
 
 def per_test_data(results) : 
     ''' Process results to print out per test data. Assumes 7 iterations for now.  '''
     out = open("results.csv", "w")
-    out.write('name,threads,median,averages,stddev,n,invstddev,time,stddev/avg,x1,x2,x3,x4,x5,x6,x7\n')
+    out.write('name,threads,median,averages,stddev,stddev/avg,x1,x2,x3,x4,x5,x6,x7\n')
     for r in results : 
-        if 'singledb' in r : 
-            for res in r['singledb'] : 
-                for key in res['nresults'] : 
-                    if key.isdigit(): 
-                        values = res['nresults'][key]['ops_per_sec_values']
-                        inv_values = [ 1/val for val in values ] 
-                        inv_mean = sum(inv_values)/len(inv_values)
-                        inv_secmom = sum((x*x for x in inv_values))
-                        inv_stddev = math.sqrt(inv_secmom - inv_mean*inv_mean)
+        for res in r['data']['results'] : 
+                for key in res['results'] :
+                    if key.isnumeric(): 
+                        values = res['results'][key]['ops_per_sec_values']
                         valstring = ','.join(str(val) for val in values)
-                        out.write(res['name']+','+ key+','+ str(res['nresults'][key]['median']) +','+ str(res['nresults'][key]["ops_per_sec"])+','+ str(res['nresults'][key]['standardDeviation'])+','+ str(res['nresults'][key]['n'])+','+str(inv_stddev) + ',' + str(res['nresults'][key]['elapsed_secs'])+','+ str(res['nresults'][key]['standardDeviation']/res['nresults'][key]['ops_per_sec']) + ',' + valstring + '\n')
+                        median = numpy.median(values)
+                        stddev = numpy.std(values)
+                        out.write(res['name']+','+ key+','+ str(median) +','+ str(res['results'][key]["ops_per_sec"])+','+ str(stddev)+','+ str(stddev/res['results'][key]['ops_per_sec']) + ',' + valstring + '\n')
 
 def per_iteration_data(results) : 
     ''' Process results to print out data per test iteration. Removes mongo-perfs default aggregation  '''
     out = open("results.iter.csv", "w")
     out.write('name,threads,median,averages,stddev,x,n,time\n')
     for r in results : 
-        if 'singledb' in r : 
-            for res in r['singledb'] : 
-                for key in res['nresults'] : 
+            for res in r['data'] : 
+                for key in res['results'] : 
                     if key.isdigit() : 
-                        values = res['nresults'][key]['ops_per_sec_values']
+                        values = res['results'][key]['ops_per_sec_values']
                         for val in values : 
-                            out.write(res['name']+','+ key+','+ str(res['nresults'][key]['median']) +','+ str(res['nresults'][key]["ops_per_sec"])+','+ str(res['nresults'][key]['standardDeviation'])+','+ str(res['nresults'][key]['n'])+','+str(val) + ',' + str(res['nresults'][key]['elapsed_secs'])+'\n')
+                            out.write(res['name']+','+ key+','+ str(res['results'][key]['median']) +','+ str(res['results'][key]["ops_per_sec"])+','+ str(res['results'][key]['standardDeviation'])+','+ str(res['results'][key]['n'])+','+str(val) + ',' + str(res['results'][key]['elapsed_secs'])+'\n')
+
+def load_data(filename='results.json') : 
+    ''' Read the input file into a json array '''
+    results = json.load(open(filename))
+    # Check if it is a list. If it isn't, put it in a list
+    if isinstance(results, list) : 
+        return results
+    else :
+        return [results]
 
 def import_data(filename='results.json', dbname='results', colname='test') : 
     ''' Import json from filename
