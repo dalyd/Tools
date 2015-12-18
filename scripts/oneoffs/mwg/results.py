f5d142c5f59d506486fe89b3037cadff498fc296
dalyd <david.daly@mongodb.com>
2015-12-18 13:43:58 -0500

    More flexibility

--- scripts/oneoffs/mwg/results.py
+++ scripts/oneoffs/mwg/results.py
@@ -11,13 +11,24 @@ def ensureFields(r, fields):
     ''' Make sure all the fields exist in M. Insert 0 if not '''
     r.update((missing, 0) for missing in set(fields).difference(r.keys()))
 
+def lookup(dic, keys):
+    if len(keys) > 1:
+        return lookup(dic.get(keys[0], {}), keys[1:])
+    return dic.get(keys[0])
+
+def CSVForField(r, key, fields, out) : 
+    ''' Create CSV File for the given key. Key is the list of keys to get to the right place '''
+    s=','
+    out.write(s.join(fields)+ '\n')
+    for rec in r: 
+        cur = lookup(rec, key)
+        ensureFields(cur, fields)
+        printCSVRecord(cur, fields, out)
+
 
 fields = ['count', 'min', 'max', 'mean', 'popStdDev']
+
 out = open("test.out", "w")
-s=','
-out.write(s.join(fields)+ '\n')
-for rec in r : 
-    ensureFields(rec['main']['insert2'],  fields)
-    printCSVRecord(rec['main']['insert2'], fields, out)
+CSVForField(r, ["main", "insert2"], fields, out)
 
 out.close()
