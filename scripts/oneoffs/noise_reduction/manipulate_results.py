9d4e6772d3196f97f75610e84ecaf8b10ca19988
dalyd <david.daly@mongodb.com>
2017-01-31 09:58:20 -0500

    Noise data manipulation working

--- scripts/oneoffs/noise_reduction/manipulate_results.py
+++ scripts/oneoffs/noise_reduction/manipulate_results.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
+'''
+One off script to fix noise result data
+'''
 
 import json
 import copy
-import numpy
+import re
 
+import itertools
+import numpy
 
 def load_data(filename='agg_results.json'):
     ''' Loads in result data'''
@@ -44,35 +49,45 @@ def recompute_ops_per_sec(thread_result):
     thread_result['it_range_to_median'] = [(float(max(it_results)) - float(min(it_results))) /
                                            float(numpy.median(it_results)) for it_results in
                                            thread_result['ops_per_sec_values']]
+    flat_array = list(itertools.chain(*thread_result['ops_per_sec_values']))
+    thread_result['all_average'] = float(numpy.average(flat_array))
+    thread_result['all_median'] = float(numpy.median(flat_array))
+    thread_result['all_variance'] = float(numpy.var(flat_array, ddof=1))
+    thread_result['all_variance_to_mean'] = (float(numpy.var(flat_array, ddof=1)) /
+                                             float(numpy.average(flat_array)))
+    thread_result['all_min'] = float(min(flat_array))
+    thread_result['all_max'] = float(max(flat_array))
+    thread_result['all_range'] = (float(max(flat_array)) - float(min(flat_array)))
+    thread_result['all_range_to_median'] = ((float(max(flat_array)) -
+                                             float(min(flat_array))) /
+                                            float(numpy.median(flat_array)))
 
 def split_results_task(task):
     ''' Split the mc results into mc and primary results for a task. Assumes 1 thread only '''
-    for name, value in task.iteritems():
+
+    pattern = re.compile('mc_')
+    # Since we are updating with new entries, need items rather than iteritems
+    for name, value in task.items():
         if pattern.match(name):
             primary_name = pattern.sub('primary_', name)
             primary_result = copy.deepcopy(value)
-            primary_test['1']['ops_per_sec_values'] = [result[5:10] for result in
-                                                       value['1']['ops_per_sec_values']]
-            recompute_ops_per_sec(primary_test['1'])
+            primary_result['1']['ops_per_sec_values'] = [result[5:10] for result in
+                                                         value['1']['ops_per_sec_values']]
+            recompute_ops_per_sec(primary_result['1'])
             value['1']['ops_per_sec_values'] = [result[0:5] for result in
                                                 value['1']['ops_per_sec_values']]
             recompute_ops_per_sec(value['1'])
             task[primary_name] = primary_result
 
+def update_results():
+    ''' update results '''
+    results = load_data()
+    for var in results.itervalues():
+        for task in var.itervalues():
+            split_results_task(task)
 
-var = results['linux-1-node-replSet']
-test = var['core_workloads_WT']['mc_latency_test_read_clat_stddev']
-mc_values = [result[0:5] for result in test['1']['ops_per_sec_values']]
-primary_values = [result[5:10] for result in test['1']['ops_per_sec_values']]
+    with open('update_results.json', 'w') as output:
+        json.dump(results, output, indent=4, sort_keys=True)
 
-primary_test = copy.deepcopy(test)
-primary_test['1']['ops_per_sec_values'] = [result[5:10] for result in test['1']['ops_per_sec_values']]
-recompute_ops_per_sec(primary_test['1'])
-test['1']['ops_per_sec_values'] = [result[0:5] for result in test['1']['ops_per_sec_values']]
-recompute_ops_per_sec(test['1'])
-
-pattern = re.compile('mc_')
-fio_test_names = [filename for filename in var['core_workloads_WT'] if pattern.match(filename)]
-primary_test_names = [pattern.sub('primary', filename) for filename in var['core_workloads_WT'] if pattern.match(filename)]
-
-task = var['core_workloads_WT']
+if __name__ == '__main__':
+    update_results()
