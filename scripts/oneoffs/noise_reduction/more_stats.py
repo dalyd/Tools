8641e335ec173704f9df06c1cf13ddfdbdb7b366
dalyd <david.daly@mongodb.com>
2017-03-15 10:33:44 -0400

    Updated for removing bad data

--- scripts/oneoffs/noise_reduction/more_stats.py
+++ scripts/oneoffs/noise_reduction/more_stats.py
@@ -5,6 +5,8 @@ Script to generate graphs
 
 from __future__ import print_function
 
+
+import itertools
 import numpy
 import pymongo
 
@@ -121,6 +123,70 @@ def dump_to_file(filename, collection):
     ''' Call generate all and dump to the fiven file'''
     with open(filename, 'w') as outfile:
         outfile.write('\n'.join(generate_all(collection)))
+
+def cleanup_outliers_indexbuild(database):
+    ''' Delete the outliers 1 by 1 '''
+    doc = database.c4pin.find_one({"test_name": "index_build-wiredTiger"})
+    del doc['ops_per_sec_values'][0][2]
+    del doc['ops_per_sec_values'][2][3]
+    doc = recalculate_from_values(doc)
+    database.c4pin.update({'_id': doc['_id']}, doc)
+    #
+    doc = database.c3pinnoht.find_one({"test_name": "index_build-wiredTiger"})
+    del doc['ops_per_sec_values'][0][3]
+    del doc['ops_per_sec_values'][4][4]
+    doc = recalculate_from_values(doc)
+    database.c3pinnoht.update({'_id': doc['_id']}, doc)
+    #
+    database.c38xlarge.find_one({"test_name": "index_build-wiredTiger"})
+    del doc['ops_per_sec_values'][1][1]
+    doc = recalculate_from_values(doc)
+    database.c38xlarge.update({'_id': doc['_id']}, doc)
+
+def recalculate_from_values(doc):
+    ''' Recalculate all computed fields '''
+    doc['it_average'] = [numpy.average(ops) for ops in doc['ops_per_sec_values']]
+    doc['it_median'] = [numpy.median(ops) for ops in doc['ops_per_sec_values']]
+    doc['it_variance'] = [numpy.var(ops, ddof=1) for ops in doc['ops_per_sec_values']]
+    # Need to zip these together
+    doc['it_variance_to_mean'] = [variance/mean for (variance, mean) in
+                                  zip(doc['it_variance'], doc['it_average'])]
+    doc['it_min'] = [min(ops) for ops in doc['ops_per_sec_values']]
+    doc['it_max'] = [max(ops) for ops in doc['ops_per_sec_values']]
+    doc['it_range'] = [maximum - minimum for (minimum, maximum) in
+                       zip(doc['it_min'], doc['it_max'])]
+    doc['it_range_to_median'] = [range_value/median for (range_value, median) in
+                                 zip(doc['it_range'], doc['it_median'])]
+    # Aggregate stats
+    doc['it_range_to_median_avg'] = numpy.average(doc['it_range_to_median'])
+    doc['it_range_to_median_max'] = max(doc['it_range_to_median'])
+    doc['it_range_to_median_min'] = min(doc['it_range_to_median'])
+    #
+    # Get all results to calculate aggregates over all data points
+    flat_array = list(itertools.chain(*doc['ops_per_sec_values']))
+    doc['all_average'] = float(numpy.average(flat_array))
+    doc['all_median'] = float(numpy.median(flat_array))
+    doc['all_variance'] = float(numpy.var(flat_array, ddof=1))
+    doc['all_variance_to_mean'] = (float(numpy.var(flat_array, ddof=1)) /
+                                   float(numpy.average(flat_array)))
+    doc['all_min'] = float(min(flat_array))
+    doc['all_max'] = float(max(flat_array))
+    doc['all_range'] = (float(max(flat_array)) - float(min(flat_array)))
+    doc['all_range_to_median'] = ((float(max(flat_array)) -
+                                   float(min(flat_array))) /
+                                  float(numpy.median(flat_array)))
+    # Top level fields
+    doc['ops_per_sec'] = [numpy.average(values) for values in doc['ops_per_sec_values']]
+    doc['average'] = numpy.average(doc['ops_per_sec'])
+    doc['median'] = numpy.median(doc['ops_per_sec'])
+    doc['variance'] = numpy.var(doc['ops_per_sec'], ddof=1)
+    doc['variance_to_mean'] = doc['variance']/doc['average']
+    doc['min'] = min(doc['ops_per_sec'])
+    doc['max'] = max(doc['ops_per_sec'])
+    doc['range'] = doc['max'] - doc['min']
+    doc['range_to_median'] = doc['range']/doc['median']
+    #
+    return doc
 
 def connect():
     '''
